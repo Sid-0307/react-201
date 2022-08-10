72dc28eda1036ed87d1ff63fd074c7271557450c
Sid-0307 <110523312+Sid-0307@users.noreply.github.com>
2022-08-10 13:17:41 +0530

    Create ErrorView.js

---
+++ src/components/ErrorView.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+  
+const ErrorView= () =>{
+    return(
+    <div class="text-center bg-dark bg-gradient text-warning">
+        <h1 class="pt-5 fs-1 ">Error 404</h1>
+        <h1 class="">Page Not Found</h1>
+    </div>
+    )
+}
+  
+export default ErrorView
